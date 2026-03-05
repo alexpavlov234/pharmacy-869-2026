a5d43083e1c5400be2defcf5fcd7834ee2597a89
Alexander Pavlov <alexpavlov234@gmail.com>
2026-03-05 10:59:58 +0200

    Created Medicine and BaseEntity classes.

---
+++ src/main/java/com/informatics/pharmacy_869_2026/data/entity/Medicine.java
@@ -0,0 +1,17 @@
+package com.informatics.pharmacy_869_2026.data.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+public class Medicine extends BaseEntity{
+    private String name;
+    private BigDecimal price;
+    private LocalDate expirationDate;
+}
