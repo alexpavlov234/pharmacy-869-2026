098f6c84d698c196fc13bac5bbacec553df6a1c1
Alexander Pavlov <alexpavlov234@gmail.com>
2026-03-05 11:00:36 +0200

    Created MedicineRepository interface which extends JpaRepository for Medicine.

---
+++ src/main/java/com/informatics/pharmacy_869_2026/data/repository/MedicineRepository.java
@@ -0,0 +1,8 @@
+package com.informatics.pharmacy_869_2026.data.repository;
+
+import com.informatics.pharmacy_869_2026.data.entity.Medicine;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicineRepository extends JpaRepository<Medicine, Long> {
+
+}
